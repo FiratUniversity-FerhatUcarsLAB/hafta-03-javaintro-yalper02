0fb0ea8a97c42ff9174923c5d06ff10f5b05baa4
yalper02 <250542014@firat.edu.tr>
2025-10-22 16:53:36 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("Y     Y");
+        System.out.println(" Y   Y ");
+        System.out.println("  Y Y  ");
+        System.out.println("   Y   ");
+        System.out.println("   Y   ");
+        System.out.println("   Y   ");
+        System.out.println("   Y   ");
+    }
+}
