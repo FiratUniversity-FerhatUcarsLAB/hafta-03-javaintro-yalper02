7b4f3f4f02aec36b29b9194390be17d4fc8d8f63
yalper02 <250542014@firat.edu.tr>
2025-10-22 15:50:40 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,8 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+
+        int sonuc = 5 * 4 * 3 * 2 * 1;
+
+        System.out.println ("5! = 5*4*3*2*1 = " + sonuc);
+    }
+}
