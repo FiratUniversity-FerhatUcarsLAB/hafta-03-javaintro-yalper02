801735fd0a1d932d4beefb36fabed1e5bc840a5e
yalper02 <250542014@firat.edu.tr>
2025-10-22 17:16:21 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,21 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        String Elma = "Elma";
+        int miktar1 = 3;
+        double fiyat1 = 5;
+
+        String Ekmek = "Ekmek";
+        int miktar2 = 2;
+        double fiyat2 = 3;
+
+        String Süt = "Süt";
+        int miktar3 = 1;
+        double fiyat3 = 7.25;
+
+        System.out.println("Ürün    Miktar  Fiyat");
+        System.out.println("-------------------------");
+        System.out.println("Elma    3       15.0 TL");
+        System.out.println("Ekmek   2       6.0 TL");
+        System.out.println("Süt     1       7.25 TL");
+    }
+}
