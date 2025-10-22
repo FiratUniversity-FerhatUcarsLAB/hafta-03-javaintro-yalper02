6c802994e65ae23a899ee2bdc6ed7d1006cdc2eb
yalper02 <250542014@firat.edu.tr>
2025-10-22 16:43:14 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKartı {
+    public static void main (String[] args) {
+        String ad = "Yiğit Alper";
+        String soyad = "Ayhan";
+        String ogrNo = "250542014";
+        String bolum = "Yazilim Muh.";
+
+        System.out.println("===================");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("===================");
+        System.out.println("Ad    : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No    : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("===================");
+    }
+}
