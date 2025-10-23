e462bb51e4abd107092069f0f6094c4ca94010ce
yalper02 <250542014@firat.edu.tr>
2025-10-23 20:37:44 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,19 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System . out . println ("Çarpım Tablosu");
+        System . out . println ("--------------");
+
+        int a = 2;
+
+        System . out . println ("2 * 1 = " + a);
+        System . out . println ("2 * 2 = " + a * 2);
+        System . out . println ("2 * 3 = " + a * 3);
+        System . out . println ("2 * 4 = " + a * 4);
+        System . out . println ("2 * 5 = " + a * 5);
+        System . out . println ("2 * 6 = " + a * 6);
+        System . out . println ("2 * 7 = " + a * 7);
+        System . out . println ("2 * 8 = " + a * 8);
+        System . out . println ("2 * 9 = " + a * 9);
+        System . out . println ("2 * 10 = " + a * 10);
+    }
+}
