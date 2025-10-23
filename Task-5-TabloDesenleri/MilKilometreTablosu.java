792e2c48bfe550c9d7bb5b62c1cf5a655179fd7a
yalper02 <250542014@firat.edu.tr>
2025-10-23 19:20:25 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,18 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        System . out . println ("Mil     Kilometre") ;
+        System . out . println ("-----------------") ;
+
+        int a1 = 1;
+        int a2 = 5;
+        int a3 = 10;
+        int a4 = 20;
+        int a5 = 50;
+
+        System . out . println (a1 + "          " + "1609" ) ;
+        System . out . println (a2 + "          " + "8045" ) ;
+        System . out . println (a3 + "         " + "16090" ) ;
+        System . out . println (a4 + "         " + "32180" ) ;
+        System . out . println (a5 + "         " + "80450" ) ;
+    }
+}
